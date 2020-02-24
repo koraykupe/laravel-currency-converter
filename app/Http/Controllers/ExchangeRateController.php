6048c472e4c2ddbeb893afa90fb0add6c5af2deb
Koray Kupe <koray.kupe@leisure-group.eu>
2020-02-24 14:58:46 +0100

    Remove unused import

--- app/Http/Controllers/ExchangeRateController.php
+++ app/Http/Controllers/ExchangeRateController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\ExchangeRate;
 use App\Http\Requests\ShowExchangeRatesRequest;
-use Illuminate\Http\Request;
 
 class ExchangeRateController extends Controller
 {
