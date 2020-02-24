531458aefcac6872426d15370163cccdb62a0e84
Koray Kupe <koray.kupe@leisure-group.eu>
2020-02-24 15:23:05 +0100

    Add auth ip migration

---
+++ database/migrations/2020_02_24_140319_create_authorization_ips_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorizationIpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authorization_ips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->ipAddress('ip_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authorization_ips');
+    }
+}
