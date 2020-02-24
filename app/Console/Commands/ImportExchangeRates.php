800d907396d5cd8e9e42dca1e6d3865273d6e55e
Koray Kupe <koray.kupe@leisure-group.eu>
2020-02-24 11:47:30 +0100

    Add guzzle request to the service to fetch exchange rates and log them

--- app/Console/Commands/ImportExchangeRates.php
+++ app/Console/Commands/ImportExchangeRates.php
@@ -2,10 +2,14 @@
 
 namespace App\Console\Commands;
 
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class ImportExchangeRates extends Command
 {
+    const SERVICE_URL = 'http://www.floatrates.com/daily/eur.json';
+
     /**
      * The name and signature of the console command.
      *
@@ -20,14 +24,17 @@ class ImportExchangeRates extends Command
      */
     protected $description = 'Imports exchange rates from an external service to our own database.';
 
+    protected $guzzleClient;
+
     /**
      * Create a new command instance.
      *
-     * @return void
+     * @param Client $guzzleClient
      */
-    public function __construct()
+    public function __construct(Client $guzzleClient)
     {
         parent::__construct();
+        $this->guzzleClient = $guzzleClient;
     }
 
     /**
@@ -37,6 +44,8 @@ class ImportExchangeRates extends Command
      */
     public function handle()
     {
-        //
+        $request = $this->guzzleClient->get(self::SERVICE_URL);
+        $response = $request->getBody();
+        Log::debug($response);
     }
 }
