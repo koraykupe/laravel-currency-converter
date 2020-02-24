c3232cb897d03e2eca242ee90f3ead3c5e4ed182
Koray Kupe <koray.kupe@leisure-group.eu>
2020-02-24 15:28:36 +0100

    Add command to add a new authorization IP

---
+++ app/Console/Commands/AddAuthorizedIp.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\AuthorizationIP;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class AddAuthorizedIp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'add:authorized-ip {ip_address}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds given ip address to the authorized ips table';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        AuthorizationIP::create(['ip_address' => $this->argument('ip_address')]);
+        Log::info($this->argument('ip_address'). ' is added to the authorization table.');
+    }
+}
